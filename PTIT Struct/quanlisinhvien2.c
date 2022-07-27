65f81af64f0b6edb490fcda7b69465541c996bed
Nguyễn Quốc Đạt <84000456+nqdat2002@users.noreply.github.com>
2022-07-27 09:26:18 +0700

    Update quanlisinhvien2.c

--- PTIT Struct/quanlisinhvien2.c
+++ PTIT Struct/quanlisinhvien2.c
@@ -1,38 +1,51 @@
 #include<stdio.h>
 #include<string.h>
-struct qlsv{
-	int ma;
-	char M[100];
-	float a,b,c;
+#include<stdlib.h>
+struct qlsv {
+	int code;
+	char name[100];
+	float p1, p2, p3, total;
 };
-int main(){
-	struct qlsv sv[100];
-	int n,a,i,b;
-	int dem=0;
-	while(1){
-	scanf("%d",&n);
-	if(n==1){
-		scanf("%d",&a);
-		printf("%d\n",a);
-		for (i=0;i<a;i++) {
-				scanf(" %[^\n]",sv[dem++].M);
-				scanf("%f%f%f",&sv[dem-1].a,&sv[dem-1].b,&sv[dem-1].c);
+int comparator(const void *p, const void *q) 
+{
+    int l = ((struct qlsv *)p)->total;
+    int r = ((struct qlsv *)q)->total; 
+    return (l - r);
+}
+int main() {
+	struct qlsv ds[100];
+	int n, q;
+	int cnt = 0;
+	while (1) {
+		scanf("%d", &q);
+		if (q == 1) {
+			scanf("%d", &n);
+			printf("%d\n", n);
+			for (int i = cnt; i < n + cnt; i++) {
+				ds[i].code = i + 1;
+				scanf(" %[^\n]", ds[i].name);
+				scanf("%f%f%f", &ds[i].p1, &ds[i].p2, &ds[i].p3);
+				ds[i].total = ds[i].p1 + ds[i].p2 + ds[i].p3;
 			}
-		continue;
-	}
-	if(n==2){
-		scanf("%d\n",&b);
-		printf("%d\n",b);
-		scanf("%[^\n]",sv[b-1].M);
-		scanf("%f%f%f",&sv[b-1].a,&sv[b-1].b,&sv[b-1].c);
-		continue;
-	}
-	if(n==3){
-		for(i=dem-1;i>=0;i--)
-		if(sv[i].a<=sv[i].b&&sv[i].b<=sv[i].c)
-		    printf("%d %s %0.1f %0.1f %0.1f\n",i+1,sv[i].M,sv[i].a,sv[i].b,sv[i].c);
+			cnt += n;
+			continue;
+		}
+		if (q == 2) {
+			scanf("%d\n", &n);
+			printf("%d\n", n);
+			scanf(" %[^\n]", ds[n - 1].name);
+			scanf("%f%f%f", &ds[n - 1].p1, &ds[n - 1].p2, &ds[n - 1].p3);
+			ds[n - 1].total = ds[n - 1].p1 + ds[n - 1].p2 + ds[n - 1].p3;
+			continue;
+		}
+		if (q == 3) {
+			for(int i = 0; i < cnt; ++i){
+				if(ds[i].p1 <= ds[i].p2 && ds[i].p2 <= ds[i].p3)
+					printf("%d %s %0.1f %0.1f %0.1f\n", 
+						ds[i].code, ds[i].name, ds[i].p1, ds[i].p2, ds[i].p3);
+			}
 			break;
 		}
-		}
+	}
 	return 0;
 }
